ea0159748af9a3f81719829dc132b09e77418047
Rajat Sharma <rajat4803@gmail.com>
2017-07-16 19:58:45 +0530

    Update LowestCommonAncestorBST.c

--- LowestCommonAncestorBST.c
+++ LowestCommonAncestorBST.c
@@ -1,4 +1,7 @@
-//The main idea of the solution is, while traversing from top to bottom, the first node n we encounter with value between n1 and n2, i.e., n1 < n < n2 or same as one of the n1 or n2, is LCA of n1 and n2 (assuming that n1 < n2). So just recursively traverse the BST in, if node’s value is greater than both n1 and n2 then our LCA lies in left side of the node, if it’s is smaller than both n1 and n2, then LCA lies on right side. Otherwise root is LCA (assuming that both n1 and n2 are present in BST)
+// The main idea of the solution is, while traversing from top to bottom, the first node n we encounter with value between n1 and n2, 
+// i.e., n1 < n < n2 or same as one of the n1 or n2, is LCA of n1 and n2 (assuming that n1 < n2). So just recursively traverse the BST
+// in, if node’s value is greater than both n1 and n2 then our LCA lies in left side of the node, if it’s is smaller than both n1 and n2,
+// then LCA lies on right side. Otherwise root is LCA (assuming that both n1 and n2 are present in BST)
 
 // Complexity O(h), h is height of the tree
 
